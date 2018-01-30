3876e201e6fdceec6fb14afafd41b87bb34450c1
Mitchell Seedorf <mitchell.seedorf@e-office.com>
2018-01-30 14:29:35 +0100

    Added MMenu + hamburger with style

--- app.js
+++ app.js
@@ -44,10 +44,14 @@ app.use('/style', express.static(path.join(__dirname, '/views/style')));
 
 // app.use('/', express.static(__dirname + '/www')); // redirect root
 // app.use('/api', api); // redirect API calls
+
 app.use(express.static(path.join(__dirname, '/node_modules')));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
+app.use('/js', express.static(__dirname + '/node_modules/jquery.mmenu/dist')); // redirect jQuery MMenu
+app.use('/css', express.static(__dirname + '/node_modules/jquery.mmenu/dist')); // redirect CSS jQuery MMenu
 
 
 
